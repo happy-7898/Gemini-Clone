d40c6ef310522feaab5c8be95687372e972fe90b
happy-7898 <singhharpreet5409@gmail.com>
2024-06-19 11:23:14 +0530

    Project Completed

---
+++ src/Components/SideBar/Sidebar.js
@@ -0,0 +1,59 @@
+import React, { useContext, useState } from 'react'
+import './Sidebar.css'
+import {assets} from '../../Assets/assets.js'
+import { Context } from '../../Context/Context.js';
+const Sidebar = () => {
+
+    const [extended,setExtended]=useState(false);
+    const {onSent,prevPrompts,setRecentPrompt,newChat}=useContext(Context)
+
+    const loadPrompt=async (prompt)=>{
+        setRecentPrompt(prompt)
+        await onSent(prompt)
+    }
+  return (
+    <div className='sidebar'>
+        <div className='top'>
+            <img onClick={()=>{setExtended(!extended)}} src={assets.menu_icon} alt='' className='menu'></img>
+            <div onClick={()=>newChat()} className='new-chat'>
+                <img src={assets.plus_icon} alt=''></img>
+                {extended?<p>New Chat</p>:null}
+                
+            </div>
+            {extended?
+                <div className='recent'>
+                    <p className='recent-title'>Recent</p>
+                    {prevPrompts.map((item,index)=>{
+                        return(
+                            <div onClick={()=>loadPrompt(item)} className='recent-entry'>
+                                <img src={assets.message_icon} alt=''></img>
+                                <p>{item.slice(0,18)}...</p>
+                            </div>
+
+                        )
+                    })}
+                </div>:null
+            
+            }
+        </div>
+        <div className='bottom'>
+            <div className='bottom-item recent-entry'>
+                <img src={assets.question_icon} alt=''></img>
+                {extended?<p>Help</p>:null}
+            </div>
+
+            <div className='bottom-item recent-entry'>
+                <img src={assets.history_icon} alt=''></img>
+                {extended?<p>Activity</p>:null}
+            </div>
+
+            <div className='bottom-item recent-entry'>
+                <img src={assets.setting_icon} alt=''></img>
+                {extended?<p>Settings</p>:null}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
